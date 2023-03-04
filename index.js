dec046c1cfdbfa31bc2d56a67fe103c0a51ec577
swyamp <coderasdhero@gmail.com>
2023-03-04 17:43:40 +0530

    init

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-console.log("hola");
+const userTab=document.querySelector("[data-userWeather]");
+const searchTab=document.querySelector("[data-searchWeather]");
+const userContainer=document.querySelector("[.weather-conatiner]");
 
+const grantAcessContainer=document.querySelector("[.grant-location-container");
+const searchForm=document.querySelector("[data-searchForm]");
+const loadingScreen=document.querySelector(".loading-container");
+const userInfoContainer=document.querySelector(".user-info-contaienr");
 
-async function showWeather(){
-    // let latitude=15.33;
-    // let longitude=74.8333;
-    try{
-        let city="goa";
-        const API_KEY="46316a53a27c8a96562dfbccaa7bf361";
-    
-        const response=await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric');
-        const data= await response.json();
-        console.log("weather data:-> ",data);
-    
-        let newPara=document.createElement('p');
-        newPara.textContent='${data?.main?.temp.toFixed(2)} C'
-        document.body.appendChild(newPara);
-        renderWeatherInfo(data);
-    }
-    catch(err){
-
-    }
-   
-}
+//initially variables neeed
+let currentTab=userTab;
+const API_KEY=
